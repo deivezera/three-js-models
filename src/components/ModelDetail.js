413fc51e0612dcd7b7f2174805174ecdce28644e
deivezera <dzabroski@gmail.com>
2022-05-24 19:44:31 -0300

    scale responsive

--- src/components/ModelDetail.js
+++ src/components/ModelDetail.js
@@ -1,16 +1,17 @@
 import { Canvas } from "@react-three/fiber";
 import { useRef } from "react";
 import { PresentationControls, ContactShadows } from "@react-three/drei";
-import { useLoader } from "@react-three/fiber";
+import { useLoader, useThree } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { useParams } from 'react-router-dom';
 
 function ModelGltf(props) {
   const ref = useRef();
-  const gltf = useLoader(GLTFLoader, `${props.modelId}.gltf`);
+  const gltf = useLoader(GLTFLoader, `/models/${props.modelId}.gltf`);
+  const { viewport } = useThree()
   
   return (
-    <group ref={ref} {...props} dispose={null}>
+    <group ref={ref} {...props} scale={(viewport.width /3) + (viewport.height/3)} dispose={null}>
       <primitive object={gltf.scene} />
     </group>
   );
@@ -26,7 +27,7 @@ function ModelDetail() {
       <PresentationControls
         global
       >
-        <ModelGltf modelId={modelId} position={[0,-1,0]} scale={5}/>
+        <ModelGltf modelId={modelId} position={[0,-1,0]}/>
       </PresentationControls>
       <ContactShadows rotation-x={Math.PI / 2} position={[0, -1.4, 0]} opacity={0.75} width={10} height={10} blur={2.6} far={2} />
     </Canvas>
