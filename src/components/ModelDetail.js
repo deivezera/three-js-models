c1ad7365e9c85b0c1f599dd681c26a8c48dfac51
deivezera <dzabroski@gmail.com>
2022-05-30 20:06:28 -0300

    autorotate

--- src/components/ModelDetail.js
+++ src/components/ModelDetail.js
@@ -11,7 +11,7 @@ function ModelGltf(props) {
   const { viewport } = useThree()
   
   return (
-    <group ref={ref} {...props} scale={(viewport.width /3) + (viewport.height/3)} dispose={null}>
+    <group ref={ref} {...props} rotation={[0,1,0]} scale={(viewport.width /3) + (viewport.height/3)} dispose={null}>
       <primitive object={gltf.scene} />
     </group>
   );
@@ -22,7 +22,6 @@ function ModelDetail() {
     <Canvas>
       <ambientLight intensity={0.5} />
       <directionalLight color="white" position={[1, 1, 1]} />
-      <directionalLight color="white" position={[-1, -1, -1]} />
       <fog attach="fog" args={['#101010', 0, 10]} />
       <spotLight position={[10, 20, 10]} angle={0.15} penumbra={1} shadow-mapSize={[512, 512]} castShadow />
       <Suspense fallback={null}>
@@ -30,7 +29,7 @@ function ModelDetail() {
           <ModelGltf modelId={modelId} position={[0,-1,0]}/>
         </Bounds>
       </Suspense>
-      <OrbitControls makeDefault minPolarAngle={0} maxPolarAngle={Math.PI / 1.75} />
+      <OrbitControls makeDefault autoRotate minPolarAngle={0} maxPolarAngle={Math.PI / 1.75} />
       <ContactShadows rotation-x={Math.PI / 2} position={[0, -1.4, 0]} opacity={0.75} width={10} height={10} blur={2.6} far={2} />
     </Canvas>
   );
